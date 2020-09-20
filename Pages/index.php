a420dd2a2e739c6dec47052e9f3e5864c0b132a8
AleshaMolodec <aleks.fandoren@gmail.com>
2020-09-20 17:26:06 +0300

    commit on 'master'

---
+++ Pages/index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Styles/style.css">
+    <link rel="stylesheet" href="../Styles/bootstrap.css">
+    <title>Милые еноты!</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg main-header">
+        <a class="navbar-brand text-light " href="#">Еноты</a>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link text-light " href="index.php">Главная</a>
+                <a class="nav-item nav-link text-light " href="Species.php">Виды</a>
+                <a class="nav-item nav-link text-light " href="#">Зоопарки</a>
+                <a class="nav-item nav-link text-light " href="#">Форум</a>
+                <a class="nav-item nav-link text-light " href="#">О нас</a>
+            </div>
+        </div>
+    </nav>
+    <div class="container-fluid">
+        <div class="row">
+                <div class="col-lg-8 col-md-12 my-auto content">
+                        <h1>
+                            Основное
+                        </h1>
+                        <p>
+                        <i>Ено́ты</i> (лат. Procyon) — род хищных млекопитающих семейства енотовых. 
+                        Представители рода — обитатели Америки. 
+                        </p>
+                        <p>
+                        На территории Евразии и, в частности, в России интродуцирован единственный вид — енот-полоскун.
+                        В России енота изначально знали по шкуркам, которые назывались «генеттовыми мехами», потому что зверек с полосатым хвостом напоминает генету. 
+                        Позднее это название превратилось в «генот» или енот. 
+                        Английское название raccoon, заимствованное и в некоторые другие языки, происходит из поухатана — одного из языков американских индейцев, 
+                        где енота называли ärähkun, от ärähkuněm, что означает «скребёт руками»; 
+                        во многих европейских и азиатских языках енота буквально называют «медведь-полоскун», «моющий медведь». 
+                        Латинское (греческого происхождения) название Procyon значит «пред-собака», «перед собакой»; 
+                        так же называется звезда Процион в созвездии Малого Пса.
+                        </p>
+                </div>
+                <div class="col-lg-4">
+                    <img src="../Resources/Raccoon_Poloskaet.jpg" class="img-fluid rounded mx-auto d-block rac" alt="Классный енот">
+                </div>
+        </div>
+        <hr class="separator" color="red" >
+        <div class="row">
+            <div class="col-lg-8 col-md-12 my-auto content">
+                <h1>
+                    Подробнее
+                </h1>
+                <p>
+                Своим общим обликом, движениями и любознательностью енот очень напоминает медведя. 
+                Недаром индейцы — аборигены Северной Америки — называли его «младшим братом медведя». 
+                В Азербайджане, куда еноты были завезены в начале 20 столетия, тоже подметили это сходство, отсюда местное название — «баладжа аи» — маленький медвежонок.
+                </p>
+                <p>
+                Люди испокон веку охотились на енотов ради меха — одного из самых дорогих мехов диких животных Северной Америки. 
+                Ещё в 19 веке шкурка енота была в Америке платёжным средством. 
+                Но интенсивная охота не привела к серьёзному уменьшению численности животных.
+                </p>
+                <p>
+                В настоящее время на своей родине, в Америке, еноты прекрасно уживаются рядом с людьми: 
+                в поисках съестного часто заходят в посёлки и на окраины городов. 
+                Этот симпатичный зверёк с лукавой, остренькой мордочкой стал героем множества комиксов и мультфильмов.
+                </p> 
+            </div>
+            <div class="col-lg-4 ">
+                    <img src="../Resources/Raccoon_Tongue.jpg" class="img-fluid mx-auto d-block rounded rac" alt="Классный енот">
+            </div>
+        </div>
+    </div>
+    <footer>
+            <div class="container-fluid text-center text-md-left foot">
+
+                <div class="row">
+
+                    <div class="col-md-6 mt-md-0 mt-3">
+
+                        <h5 class="text-uppercase">Какая-то общая информация</h5>
+                        <p>Сайт очень классный, мне нравится. Его, кстати, сделал очень крутой человек, в 581 группе учится, вот.</p>
+
+                    </div>
+
+                    <hr class="clearfix w-100 d-md-none pb-3">
+
+                        <div class="col-md-3 mb-md-0 mb-3">
+
+                            <h5 class="text-uppercase">Ссылки</h5>
+
+                            <ul class="list-unstyled">
+                            <li>
+                                <a class="text-white" href="#!"><img class="link-icon" src="../Resources/Vkontakte.png"> Вконтакте </a>
+                            </li>
+                            </ul>
+
+                        </div>
+
+                </div>
+            </div>
+    </footer>
+</body>
+</html>
